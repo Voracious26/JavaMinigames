ce4e6a4f724bdc0f673a19ea180e663db16877e2
Voracious26 <37031448+Voracious26@users.noreply.github.com>
2020-10-18 13:41:21 -0400

    Update TicTacToe.java to have text area
    Text area is JTextArea infoArea. Used to display game instructions and win/loss/tie messages.

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -9,6 +9,12 @@ public class TicTacToe {
     // GUI elements
     public JFrame mainFrame;   
     public JPanel gameBoardPanel;
+    public JTextArea infoArea;
+    // infoArea text
+    private String startMsg = "It's Tic Tac Toe time. Click on a square to make your turn when you're ready.";
+    private String winMsg = "You won the game! Congratulations.";
+    private String lossMsg = "You lost. May you never forget it.";
+    private String tieMsg = "It's a tie. How unexpected.";
     // game variables
     public boolean gameOver = false;
     public int boardSize = 3; 
@@ -16,14 +22,21 @@ public class TicTacToe {
     
     public TicTacToe(JFrame menuFrame){          
         gameBoard = new Board(this);
+        infoArea = new JTextArea (1, 30);
+        infoArea.setText(startMsg);
         
         mainFrame = menuFrame;
         gameBoardPanel = generateBoardPanel();
         mainFrame.setSize(500,500);        
-        mainFrame.add(gameBoardPanel);
+        mainFrame.add(gameBoardPanel, BorderLayout.CENTER);
+        mainFrame.add(infoArea, BorderLayout.SOUTH);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
         
+    }
+    
+    public void setInfoMsg(String msg){
+        infoArea.setText(msg);
     }
     
     public JPanel generateBoardPanel(){
@@ -35,6 +48,7 @@ public class TicTacToe {
     
     public void returnToMainMenu(){
         mainFrame.remove(gameBoardPanel);
+        mainFrame.remove(infoArea);
         JavaMinigames.loadMainFrame();
     }
 }
